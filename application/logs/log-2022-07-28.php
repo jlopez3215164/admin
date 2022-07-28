98d3bdd0c4c7dd510cb6c6cdf0b553f680091969
JOSE <jlopez.unix@gmail.com>
2022-07-28 10:03:13 +0200

    Ajustes

---
+++ application/logs/log-2022-07-28.php
@@ -0,0 +1,9 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2022-07-28 03:56:17 --> Severity: 8192 --> Required parameter $minutes follows optional parameter $store C:\xampp\htdocs\admin\modules\omni_sales\helpers\Omni_sales_helper.php 40
+ERROR - 2022-07-28 03:56:17 --> Severity: Warning --> require(C:\xampp\htdocs\admin\modules\surveys/vendor/autoload.php): Failed to open stream: No such file or directory C:\xampp\htdocs\admin\modules\surveys\surveys.php 12
+ERROR - 2022-07-28 03:56:17 --> Severity: error --> Exception: Failed opening required 'C:\xampp\htdocs\admin\modules\surveys/vendor/autoload.php' (include_path='\xampp\php\PEAR') C:\xampp\htdocs\admin\modules\surveys\surveys.php 12
+ERROR - 2022-07-28 03:59:10 --> Severity: Warning --> require(C:\xampp\htdocs\admin\modules\surveys/vendor/autoload.php): Failed to open stream: No such file or directory C:\xampp\htdocs\admin\modules\surveys\surveys.php 12
+ERROR - 2022-07-28 03:59:10 --> Severity: error --> Exception: Failed opening required 'C:\xampp\htdocs\admin\modules\surveys/vendor/autoload.php' (include_path='\xampp\php\PEAR') C:\xampp\htdocs\admin\modules\surveys\surveys.php 12
+ERROR - 2022-07-28 03:59:55 --> Severity: Warning --> require(C:\xampp\htdocs\admin\modules\surveys/vendor/autoload.php): Failed to open stream: No such file or directory C:\xampp\htdocs\admin\modules\surveys\surveys.php 12
+ERROR - 2022-07-28 03:59:55 --> Severity: error --> Exception: Failed opening required 'C:\xampp\htdocs\admin\modules\surveys/vendor/autoload.php' (include_path='\xampp\php\PEAR') C:\xampp\htdocs\admin\modules\surveys\surveys.php 12
