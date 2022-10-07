79af5c0bf47ba105d9c279894c4478d47a7bfe99
JOSE <jlopez.unix@gmail.com>
2022-10-07 13:57:32 -0400

    Ajustes

--- changes.sql
+++ changes.sql
@@ -5,11 +5,17 @@ ALTER TABLE
     tblgoods_transaction_detail
 ADD
     COLUMN consolidate INT
-
 SET
     sql_mode = '';
 
 ALTER TABLE
     tblitems
 ADD
-    COLUMN price_sale NUMERIC(18,4)
+    COLUMN price_sale NUMERIC(18, 4)
+SET
+    sql_mode = '';
+
+ALTER TABLE
+    tblitems
+ADD
+    COLUMN date_price DATETIME
