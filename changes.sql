c78caa9ed761756bc49a123cf31f94f618e70fb0
jlopez3215164 <97767761+jlopez3215164@users.noreply.github.com>
2023-03-23 09:16:53 -0400

    Ajuste

--- changes.sql
+++ changes.sql
@@ -19,3 +19,28 @@ ALTER TABLE
     tblitems
 ADD
     COLUMN date_price DATETIME;
+
+    -- orion_zeus.tbl_orion_pos_products definition
+
+CREATE TABLE `tbl_orion_pos_products` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `admin_id` bigint unsigned DEFAULT NULL,
+  `pos_id` bigint unsigned DEFAULT NULL,
+  `date` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- orion_zeus.tbl_orion_pos definition
+
+CREATE TABLE `tbl_orion_pos` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `name` text COLLATE utf8mb4_general_ci,
+  `ip` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `port` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `database` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `user_database` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `password_database` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `status` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `date` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
