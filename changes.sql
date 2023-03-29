cd65a39e8034963abf42ffb8faffdb90b32681db
jlopez3215164 <97767761+jlopez3215164@users.noreply.github.com>
2023-03-29 09:43:36 -0400

    Ajustes

--- changes.sql
+++ changes.sql
@@ -73,3 +73,5 @@ CREATE TABLE `tbl_orion_pos_detail_sale` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 ALTER TABLE tbl_orion_pos_products ADD price_from_pos int NULL;
+
+ALTER TABLE tbl_orion_pos_products ADD last_sync datetime NULL;
