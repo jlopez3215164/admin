d79975338c6a639f95871ec50cd64188387d66cb
jlopez3215164 <jlopez.xetux@gmail.com>
2024-07-22 18:18:34 -0400

    SQL

---
+++ orion_zeus.sql
@@ -0,0 +1,8 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+<div class="alert alert-danger" role="alert"><img src="themes/dot.gif" title="" alt="" class="icon ic_s_error"> TypeError: PhpMyAdmin\Export::exportDatabase(): Argument #3 ($whatStrucOrData) must be of type string, null given, called in C:\xampp\phpMyAdmin\libraries\classes\Controllers\Export\ExportController.php on line 548</div></body></html>
